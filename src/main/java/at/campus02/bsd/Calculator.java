2b752bd1ce44ca6731e1bacd4d3ba8aa20d19c15
Claudia Cañamero Ballestar <12003750@edu.campus02.at>
2026-06-28 09:52:38 +0200

    adding not log datei

--- src/main/java/at/campus02/bsd/Calculator.java
+++ src/main/java/at/campus02/bsd/Calculator.java
@@ -1,28 +1,42 @@
 package at.campus02.bsd;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Calculator {
+    private static Logger log = LogManager.getLogger(Main.class);
 
     public double add(double number1, double number2){
         double result = number1 + number2;
+        log.debug("ADD METHOD: parameters given: "+number1+","+number2);
+        log.debug("ADD METHOD: result: "+result);
         return result;
     }
 
     public double subtract(double number1, double number2){
         double result = number1 - number2;
+        log.debug("SUBSTRACT METHOD: parameters given: "+number1+","+number2);
+        log.debug("SUBSTRACT METHOD: result: "+result);
         return result;
     }
 
     public double divide(double number1, double number2){
-        if(number2 != 0) {
-            double result = number1 / number2;
-            return result;
-        }else{
+        if(number2 == 0) {
+            log.error("DIVIDE METHOD: parameters given: "+number1+","+number2+" impossible to divide to 0");
             return -1;
+        }else{
+
+            double result = number1 / number2;
+            log.debug("DIVIDE METHOD: parameters given: "+number1+","+number2);
+            log.debug("DIVIDE METHOD: result: "+result);
+            return result;
         }
     }
 
     public double multiply(double number1, double number2){
         double result = number1 * number2;
+        log.debug("MULTIPLY METHOD: parameters given: "+number1+","+number2);
+        log.debug("MULTIPLY METHOD: result: "+result);
         return result;
     }
 }
--- src/main/java/at/campus02/bsd/Main.java
+++ src/main/java/at/campus02/bsd/Main.java
@@ -1,13 +1,28 @@
 package at.campus02.bsd;
 
-public class Main {
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+class Main {
+     private static Logger log = LogManager.getLogger(Main.class);
+
     static void main() {
-       Calculator calculator = new Calculator();
+       Calculator calculator = new at.campus02.bsd.Calculator();
        double add = calculator.add(1,2);
        double subs = calculator.subtract(1,2);
        double mult = calculator.multiply(1,2);
        double div = calculator.divide(1,2);
+       double add2 = calculator.add(2,4);
+       double sub2 = calculator.subtract(5,9);
+       double mult2 = calculator.multiply(1,9.5);
+       double div2 = calculator.divide(17,0);
         System.out.println(add+" "+subs+" "+mult+" "+div);
+        System.out.println(add2+" "+sub2+" "+mult2+" "+div2);
         System.out.println("Claudia Cañamero Ballestar");
+
+        log.info("info");
+        log.error("error");
+
     }
 }
